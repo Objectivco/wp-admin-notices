5beadee4e092ac67a9d8c4bbc7850557b27f85a3
Clifton Griffin <clif@objectiv.co>
2024-01-04 15:37:36 -0500

    Revert "Add image option"
    This reverts commit 1369bc14e0b5455073c37862270210c15c817e88.

--- src/Notice.php
+++ src/Notice.php
@@ -248,13 +248,7 @@ class Notice {
 	 * @return string
 	 */
 	public function get_message() {
-		$message = sprintf( '<div class="pressmodo-notice-content">%s</div>', wpautop( wp_kses_post( $this->message ) ) );
-
-		if ( $this->options['image'] ) {
-			$image = sprintf( '<div class="pressmodo-notice-image">%</div>', $this->options['image'] );
-		}
-
-		return $image . $message;
+		return wpautop( wp_kses_post( $this->message ) );
 	}
 
 	/**
